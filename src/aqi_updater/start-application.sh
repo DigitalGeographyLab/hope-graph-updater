8fd5d119e6ae2aa727c7d8c0766b1f0014b40cc5
Joose Helle <joose.helle@gmail.com>
2020-07-02 11:27:53 +0300

    Adjust build

--- src/aqi_updater/start-application.sh
+++ src/aqi_updater/start-application.sh
@@ -3,8 +3,8 @@
 if [[ ! -z "${RUN_DEV}" && "${RUN_DEV}" = "True" ]]; then
   echo "Starting AQI updater with small graph (dev)"
   export GRAPH_SUBSET="True"
-  python aqi_updater_app.py
+  python -u aqi_updater_app.py
 else
   echo "Starting AQI updater with full graph (prod)"
-  python aqi_updater_app.py
+  python -u aqi_updater_app.py
 fi
